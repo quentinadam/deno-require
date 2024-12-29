1dc5e207037686ebae60d2a8ea31c2a523a3117f
Quentin Adam <quentinadam@gmail.com>
2024-12-29 22:04:17 +0100

    Generate jsr.json and package.json from deno.json. Update @quentinadam/assert dependency.

--- scripts/check-version-numbers.ts
+++
@@ -1,8 +0,0 @@
-const jsrVersion = JSON.parse(Deno.readTextFileSync('deno.json')).version;
-const npmVersion = JSON.parse(Deno.readTextFileSync('package.json')).version;
-if (jsrVersion !== npmVersion) {
-  console.error(`Version mismatch in deno.json (${jsrVersion}) and package.json (${npmVersion})`);
-  Deno.exit(1);
-} else {
-  console.log('Version numbers match');
-}
